95044c8d5a4807069830a0469fcfb2049111f4f0
Kamesh255 <kameshhedau19@gmail.com>
2021-11-08 16:12:14 +0530

    completed
    complet

--- food/navbar/navbar.js
+++ food/navbar/navbar.js
@@ -1,8 +1,8 @@
  function navbar(){
      return  `<h2><a href="menu.html">Menu </a></h2>
      <div id="options">
-         <h2><a href="login.html">Login</a></h2>
-         <h2><a href="Signup.html">Signup</a></h2>
+         <h2><a href="signin.html">Signin/Login</a></h2>
+         <h2><a href="index.html">Sign-out</a></h2>
      </div>`
  }
  export default navbar
