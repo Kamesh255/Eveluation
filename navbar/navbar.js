df0c24b8f5dc46f521a099fb469601f0b983c72e
Kamesh255 <91534792+Kamesh255@users.noreply.github.com>
2021-11-08 16:25:41 +0530

    Add files via upload

---
+++ navbar/navbar.js
@@ -0,0 +1,8 @@
+ function navbar(){
+     return  `<h2><a href="menu.html">Menu </a></h2>
+     <div id="options">
+         <h2><a href="signin.html">Signin/Login</a></h2>
+         <h2><a href="index.html">Sign-out</a></h2>
+     </div>`
+ }
+ export default navbar
